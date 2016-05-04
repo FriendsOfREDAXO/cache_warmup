3a42685b646d86e7dca25dfa3e8385c0f6ed194c
schuer <ds@decaf.de>
2016-05-05 00:08:21 +0200

    update lang files

--- pages/system.cache-warmup.php
+++ pages/system.cache-warmup.php
@@ -3,7 +3,7 @@
 $content = '';
 
 $content .= '<p>' . $this->i18n('description') . '</p>';
-$content .= '<p><a class="btn btn-primary cache-warmup__button-start" href="' . rex_url::backendPage("cache-warmup/warmup") . '" target="CacheWarmupWindow">' . $this->i18n('button') . ' <i class="fa fa-external-link" aria-hidden="true"></i></a></p>';
+$content .= '<p><a class="btn btn-primary cache-warmup__button-start" href="' . rex_url::backendPage("cache-warmup/warmup") . '" target="CacheWarmupWindow">' . $this->i18n('button-start') . ' <i class="fa fa-external-link" aria-hidden="true"></i></a></p>';
 
 
 $fragment = new rex_fragment();
