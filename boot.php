d3f1965d9542a54367d573c91c6ba4da6881b5dc
schuer <ds@decaf.de>
2016-05-08 22:05:57 +0200

    add some config to boot

--- boot.php
+++ boot.php
@@ -1,4 +1,14 @@
 <?php
+
+// number of images to generate per single request
+// increase to speed up (reduces number of requests but extends script time)
+$this->setConfig('chunkSizeImages', 3);
+
+// number of pages to generate per single request
+// increase to speed up (reduces number of requests but extends script time)
+$this->setConfig('chunkSizePages', 50);
+
+// inject addon ressources
 if (rex::isBackend() && rex::getUser()) {
     rex_view::addCssFile($this->getAssetsUrl('css/cache-warmup.css'));
     rex_view::addJsFile($this->getAssetsUrl('js/cache-warmup.js'));
