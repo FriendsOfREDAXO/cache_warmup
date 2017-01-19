a85e093497dc1350592acc3f869a464eaac1a56a
Markus Staab <maggus.staab@googlemail.com>
2017-01-19 11:35:38 +0100

    improve readability (#34)
    * improve readability
    
    * fixed typo

--- lib/generator_pages.php
+++ lib/generator_pages.php
@@ -16,16 +16,17 @@ class cache_warmup_generator_pages extends cache_warmup_generator
     {
         if (rex_addon::get('structure')->isAvailable()) {
             foreach ($items as $item) {
+                list($article_id, $clang) = $item;
 
                 // generate content
-                $article = new rex_article_content($item[0], $item[1]);
-                $content = $article->getArticle();
+                $article = new rex_article_content($article_id, $clang);
+                $article->getArticle();
 
                 // generate meta
-                rex_article_cache::generateMeta($item[0], $item[1]);
+                rex_article_cache::generateMeta($article_id, $clang);
 
                 // generate lists
-                rex_article_cache::generateLists($item[0]);
+                rex_article_cache::generateLists($article_id);
             }
         }
         return $items;
