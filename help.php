b3804fc304b713c785cc714434025de14701791a
schuer <ds@decaf.de>
2017-01-20 17:29:56 +0100

    remove `help.php` to show README instead

--- help.php
+++
@@ -1,5 +0,0 @@
-<h3>Cache-Warmup</h3>
-
-<p>Generiert den Cache vorab, so dass die Website bereits beim Erstaufruf performant läuft.</p>
-
-<p><a href="https://github.com/FriendsOfREDAXO/cache_warmup">github.com/FriendsOfREDAXO/cache_warmup</a></p>
