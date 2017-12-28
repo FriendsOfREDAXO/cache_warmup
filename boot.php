241fc440f77d401345f04535a4c7a3e09149bd26
Markus Staab <maggus.staab@googlemail.com>
2017-12-28 20:13:45 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -46,8 +46,8 @@ rex_extension::register('PACKAGES_INCLUDED', function (rex_extension_point $ep) 
     }
 }, rex_extension::EARLY);
 
-// inject addon ressources
-if (rex::isBackend() && rex::getUser() && rex_be_controller::getCurrentPagePart(1) == 'cache_warmup') {
+// inject addon ressources, for both cache_warmup/generator and system/cache_warmup
+if (rex::isBackend() && rex::getUser() && strpos(rex_be_controller::getCurrentPage(), 'cache_warmup') !== false) {
 
     if (rex_be_controller::getCurrentPagePart(2) == 'warmup') {
         rex_view::addJsFile($this->getAssetsUrl('js/handlebars.min.js'));
