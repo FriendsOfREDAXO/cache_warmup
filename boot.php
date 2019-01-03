c61d09215e9ae37ff22805f992c2f9c641c06beb
schuer <ds@decaf.de>
2019-01-03 01:13:17 +0100

    refactor boot.php
    - set/update config on warmup page (popup) now
    - ref $this
    - code style, clean up

--- boot.php
+++ boot.php
@@ -1,5 +1,8 @@
 <?php
-if (rex_be_controller::getCurrentPage() == 'system/cache_warmup/start') {
+/** @var rex_addon $this */
+
+// set/update config on warmup page (popup)
+if (rex_be_controller::getCurrentPage() == 'cache_warmup/warmup') {
 
     // chunk size config
     // min: min number of items to generate per request
@@ -37,16 +40,7 @@ if (rex_be_controller::getCurrentPage() == 'system/cache_warmup/start') {
     }
 }
 
-
-// switch REDAXO to frontend mode before generating cache files
-// this is essential to include content modification by addons, e.g. slice status on/off
-rex_extension::register('PACKAGES_INCLUDED', function (rex_extension_point $ep) {
-    if (rex_be_controller::getCurrentPage() == 'cache_warmup/generator') {
-        rex::setProperty('redaxo', false);
-    }
-}, rex_extension::EARLY);
-
-// inject addon ressources, for both cache_warmup/warmup and system/cache_warmup*
+// inject addon ressources
 if (rex::isBackend() && rex::getUser() && strpos(rex_be_controller::getCurrentPage(), 'cache_warmup') !== false) {
 
     if (rex_be_controller::getCurrentPagePart(2) == 'warmup') {
@@ -57,3 +51,11 @@ if (rex::isBackend() && rex::getUser() && strpos(rex_be_controller::getCurrentPa
     rex_view::addCssFile($this->getAssetsUrl('css/cache-warmup.css'));
     rex_view::addJsFile($this->getAssetsUrl('js/cache-warmup.js'));
 }
+
+// switch REDAXO to frontend mode before generating cache files
+// this is essential to include content modification by addons, e.g. slice status on/off
+rex_extension::register('PACKAGES_INCLUDED', function (rex_extension_point $ep) {
+    if (rex_be_controller::getCurrentPage() == 'cache_warmup/generator') {
+        rex::setProperty('redaxo', false);
+    }
+}, rex_extension::EARLY);
