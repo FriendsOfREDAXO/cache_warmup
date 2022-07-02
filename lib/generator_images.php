abfd72652c4c428ed42813c9516c0ded145796b9
schuer <ds@decaf.de>
2022-07-02 18:07:32 +0200

    compat: remove fallback for media_manager < 2.3.0 (REX 5.4)

--- lib/generator_images.php
+++ lib/generator_images.php
@@ -16,13 +16,7 @@ class cache_warmup_generator_images extends cache_warmup_generator
         if (rex_addon::get('media_manager')->isAvailable()) {
             foreach ($items as $item) {
                 [$image, $type] = $item;
-
-                if (method_exists('rex_media_manager', 'create')) {
-                    rex_media_manager::create($type, $image);
-                } else {
-                    // use fallback for media_manager < 2.3.0
-                    cache_warmup_media_manager::init($image, $type);
-                }
+                rex_media_manager::create($type, $image);
             }
         }
         return $items;
